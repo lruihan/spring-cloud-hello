1d74e68635a866b671fd2c58ed6f625124c3c9da
Rissy Lin <lsj.ruihan@gmail.com>
2018-01-06 23:15:59 +0800

    First commit

---
+++ src/main/java/com/rissy/springcloudhello/SpringCloudHelloApplication.java
@@ -0,0 +1,12 @@
+package com.rissy.springcloudhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudHelloApplication.class, args);
+	}
+}
